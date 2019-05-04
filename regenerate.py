b03a888a9794393ac9639ca5fe29d58ae64e3666
amlmn <3488229+amlmn@users.noreply.github.com>
2019-05-04 14:57:18 -0700

    Also regenerate css files

--- regenerate.py
+++ regenerate.py
@@ -6,6 +6,7 @@ import argparse
 import glob
 import logging
 import os
+import shutil
 import subprocess
 
 logger = logging.getLogger(__name__)
@@ -27,10 +28,8 @@ DEFAULT_PANDOC_FLAGS = [
 
 def run(*args) -> str:
   logging.debug('Running command: %s', args)
-  result = subprocess.run(args,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE,
-                          encoding='utf-8')
+  result = subprocess.run(
+    args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
   logging.debug('\nStdout: %s\nStderr: %s', result.stdout, result.stderr)
   return result
 
@@ -46,8 +45,7 @@ def GenerateHtml(pandoc_flags: List[str], doc_paths: List[str],
 
   for post_path in post_paths:
     output_dir = os.path.join(site_root, os.path.splitext(post_path)[0])
-    if not os.path.exists(output_dir):
-      os.makedirs(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
     output_path = os.path.join(output_dir, 'index.html')
     output(post_path, output_path)
   for doc_path in doc_paths:
@@ -56,29 +54,44 @@ def GenerateHtml(pandoc_flags: List[str], doc_paths: List[str],
     output(doc_path, output_path)
 
 
+def CopyCss(css_dir: str, site_root: str):
+  output_dir = os.path.join(site_root, css_dir)
+  os.makedirs(output_dir, exist_ok=True)
+  input_files = glob.iglob(os.path.join(css_dir, '*.css'))
+  for input_file in input_files:
+    shutil.copy2(input_file, output_dir)
+
+
 def ParseArgs():
   parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  parser.add_argument('--posts',
-                      type=str,
-                      default='algos',
-                      help='directory relative to the repo root')
-  parser.add_argument('--docs',
-                      type=str,
-                      default='docs',
-                      help='directory relative to the repo root')
-  parser.add_argument('--site_root',
-                      type=str,
-                      default='build',
-                      help='output dir, relative to the repo root')
-  parser.add_argument('--pandoc_flags',
-                      type=str,
-                      default='',
-                      help='passed verbatim to pandoc')
-  parser.add_argument('--log_level',
-                      type=str,
-                      help='controls the script\'s logging level',
-                      default='ERROR')
+  parser.add_argument(
+    '--posts',
+    type=str,
+    default='algos',
+    help='directory relative to the repo root')
+  parser.add_argument(
+    '--docs',
+    type=str,
+    default='docs',
+    help='directory relative to the repo root')
+  parser.add_argument(
+    '--css',
+    type=str,
+    default='css',
+    help='directory relative to the repo root')
+  parser.add_argument(
+    '--site_root',
+    type=str,
+    default='build',
+    help='output dir, relative to the repo root')
+  parser.add_argument(
+    '--pandoc_flags', type=str, default='', help='passed verbatim to pandoc')
+  parser.add_argument(
+    '--log_level',
+    type=str,
+    help='controls the script\'s logging level',
+    default='ERROR')
   return parser.parse_args()
 
 
@@ -97,6 +110,7 @@ def main():
   post_paths = glob.iglob(os.path.join(args.posts, '*.md'))
   pandoc_flags = [arg for arg in args.pandoc_flags.split(' ') if arg]
   GenerateHtml(pandoc_flags, doc_paths, post_paths, args.site_root)
+  CopyCss(args.css, args.site_root)
 
 
 if __name__ == '__main__':
