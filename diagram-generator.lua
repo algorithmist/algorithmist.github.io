cbc6405d21f15612a6cf21950eba8270a7f7208a
amlmn <3488229+amlmn@users.noreply.github.com>
2019-06-28 00:23:39 -0700

    Don't nest SVGs

--- diagram-generator.lua
+++ diagram-generator.lua
@@ -18,25 +18,7 @@ function CodeBlock(block)
 
   local svg = pandoc.pipe('dot', {'-Tsvg'}, block.text)
 
-  local attrs = {}
-  -- Hack to always have a space before the concatenated string.
-  table.insert(attrs, '')
-  if block.identifier then
-    table.insert(attrs, 'id="' .. block.identifier .. '"')
-  end
-  if block.attributes['style'] then
-    table.insert(attrs, 'style="' .. block.attributes['style'] .. '"')
-  end
-  --if block.attributes['width'] then
-  --  table.insert(attrs, 'width="' .. block.attributes['width'] .. '"')
-  --end
-  --if block.attributes['height'] then
-  --  table.insert(attrs, 'height="' .. block.attributes['height'] .. '"')
-  --end
-  table.insert(attrs, 'height="100%"')
-  table.insert(attrs, 'width="100%"')
-
-  return pandoc.RawBlock('html', '<svg' .. table.concat(attrs, ' ') .. '>' .. svg .. '</svg>')
+  return pandoc.RawBlock('html', svg)
 end
 
 return {
