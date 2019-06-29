c90aa77d630a7be53d38b01ffba62517b82bbc77
Wil Thomason <wil.thomason@gmail.com>
2019-06-29 16:16:17 -0400

    Format and remove print-by-default in Lua filter

--- diagram-generator.lua
+++ diagram-generator.lua
@@ -1,21 +1,20 @@
 function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+  if type(o) == 'table' then
+    local s = '{ '
+    for k,v in pairs(o) do
+      if type(k) ~= 'number' then k = '"'..k..'"' end
+      s = s .. '['..k..'] = ' .. dump(v) .. ','
+    end
+    return s .. '} '
+  else
+    return tostring(o)
+  end
 end
 
 function CodeBlock(block)
   -- Shows up with --log_level=DEBUG
-  print('---')
-  print(dump(block))
-
+  -- print('---')
+  -- print(dump(block))
   local svg = pandoc.pipe('dot', {'-Tsvg'}, block.text)
 
   return pandoc.RawBlock('html', svg)
