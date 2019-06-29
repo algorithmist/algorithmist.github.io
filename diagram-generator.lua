0d07145e623d42236892cd054b0ecc8d2c85a811
Wil Thomason <wil.thomason@gmail.com>
2019-06-29 16:16:42 -0400

    Add div wrapper, figure wrapper, caption, and proper ID setting for SVG

--- diagram-generator.lua
+++ diagram-generator.lua
@@ -16,8 +16,27 @@ function CodeBlock(block)
   -- print('---')
   -- print(dump(block))
   local svg = pandoc.pipe('dot', {'-Tsvg'}, block.text)
+  local caption = nil
+  if block.attributes["caption"] then
+    caption = block.attributes['caption']
+  end
 
-  return pandoc.RawBlock('html', svg)
+  local figObj = pandoc.RawBlock('html', '<figure>\n\t' .. svg)
+  if caption then
+    figObj.text = figObj.text .. '\t<figcaption>' .. caption .. '</figcaption>\n'
+  end
+  
+  figObj.text = figObj.text .. '</figure>'
+  local divObj = pandoc.Div(figObj)
+
+  if block.attributes['name'] then
+    divObj.attributes['name'] = block.attributes['name']
+    divObj.identifier = 'fig:' .. block.attributes['name']
+  end
+
+  divObj.classes[#divObj.classes + 1] = 'fig'
+
+  return divObj
 end
 
 return {
